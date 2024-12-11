d9559c4cae77539c88981521f48b9caf969e2607
sathru0103 <142097194+sathru0103@users.noreply.github.com>
2024-12-12 01:55:01 +0530

    Update InvoiceRepository.java

--- src/main/java/com/crm/crm_web_app/repository/InvoiceRepository.java
+++ src/main/java/com/crm/crm_web_app/repository/InvoiceRepository.java
@@ -1,13 +1,17 @@
 package com.crm.crm_web_app.repository;
 
 import com.crm.crm_web_app.entity.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    @Query("SELECT SUM(i.amount) FROM Invoice i WHERE i.status = 'PAID'")
-    BigDecimal calculateTotalRevenue();
+	
+	List<Invoice> findByCustomerId(Long customerId);
+	
+	List<Invoice> findByStatus(String status);
+
 }
 
