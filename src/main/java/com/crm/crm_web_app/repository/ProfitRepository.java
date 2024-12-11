2f034fd80242617c453f1403650ec0471b6daf45
sathru0103 <142097194+sathru0103@users.noreply.github.com>
2024-12-12 01:57:03 +0530

    Update ProfitRepository.java

--- src/main/java/com/crm/crm_web_app/repository/ProfitRepository.java
+++ src/main/java/com/crm/crm_web_app/repository/ProfitRepository.java
@@ -1,8 +1,16 @@
 package com.crm.crm_web_app.repository;
 
+import com.crm.crm_web_app.entity.Investment;
 import com.crm.crm_web_app.entity.Profit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ProfitRepository extends JpaRepository<Profit, Long> {
-
+    List<Profit> findByDateBetween(LocalDate start, LocalDate end);
+    Optional<Profit> findByInvestment(Investment investment);
 }
